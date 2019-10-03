28188fa88c15c647006b754a9228b9cad2ddc38c
Jose Romero <joseromero@unilene.com>
2019-10-03 09:36:14 -0500

    Activando el LOOP de WordPress en el Content.php. Se añade H2 a Footer y Sidebar para diferenciar las secciones

--- content.php
+++ content.php
@@ -1,3 +1,9 @@
     <article>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore maiores id minima culpa amet aperiam sed adipisci recusandae nisi at quae dolores accusamus quod quis iusto, doloremque nulla? Placeat, facere.</p>
+        <h1>Primer Curso</h1>
+        <h2>Contenido</h2>
+        <?php if(have_posts()):while(have_posts()):the_post();?>
+        <p>Contenido de la entrada</p>
+        <?php endwhile; else:?>
+        <p>El contenido solicitado no existe</p>
+        <?php endif;?>
     </article>
--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
     </main>
     <footer>
         <div>
+            <h2>Footer</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quis qui enim autem voluptatum recusandae fugit dignissimos doloremque facere fugiat nemo, eos nulla ea. Ipsa est architecto aliquid repudiandae iste!</p>
         </div>
     </footer>

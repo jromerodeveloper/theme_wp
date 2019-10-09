3cec57c60a282e165beadf18fe3c7aae98e1b848
Jose Romero <joseromero@unilene.com>
2019-10-09 08:52:51 -0500

    Se agregaron las funciones en el custom-admin de la carpeta inc para eliminar opciones del menu del dashboard y la para remover opciones de la barra de admimistrador superior del dashboard

--- inc/custom-admin.php
+++ inc/custom-admin.php
@@ -33,3 +33,50 @@ if (!function_exists('themeWP_add_editor_styles')):
 endif;
   
 add_action('after_setup_theme','themeWP_add_editor_styles');
+
+// Función para eliminar opciones del menu del Dashboard
+if (!function_exists('mawt_admin_menu')):
+    function mawt_admin_menu(){
+      //remove_menu_page('edit.php'); // Entradas
+      //remove_menu_page('upload.php'); // Multimedia
+      //remove_menu_page('link-manager.php'); // Enlaces
+      //remove_menu_page('edit.php?post_type=page'); // Páginas
+      //remove_menu_page('edit-comments.php'); // Comentarios
+      //remove_menu_page('themes.php'); // Apariencia
+      //remove_menu_page('plugins.php'); // Plugins
+      //remove_menu_page('users.php'); // Usuarios
+      //remove_menu_page('tools.php'); // Herramientas
+      //remove_menu_page('options-general.php'); // Ajustes
+    }
+  endif;
+  add_action('admin_menu','mawt_admin_menu');
+
+  if (!function_exists('mawt_before_admin_bar')):
+    function mawt_before_admin_bar(){
+      global $wp_admin_bar;
+      /*
+        search:para eliminar la caja de búsqueda
+        comments:Para eliminar el aviso de comentarios
+        updates:Eliminar el aviso de actualizaciones
+        edit:Elimina editar entrada y páginas
+        get-shortlink:Proporciona un enlace corto a esa página/post
+        my-sites:Elimina el menu my sitios, si utilizas la función multisitios de wordpress
+        site-name:Elimina el nombre de la web
+        wp-logo:Elimina el logo(y el sub Menú)
+        my-account:Elimina los enlaces a su cuenta. El ID depende de si usted tiene Avatar habilitado o no.
+        view-site:Elimina el sub menú que aparece al pasar el cursor sobre el nombre de la web
+        about:Elimina el enlace “Sobre WordPress
+        wporg:Elimina el enlace a wordpress.org
+        documentation:Elimina el enlace a la documentación oficial(Codex)
+        support-forums:Elimina el enlace a los foros de ayuda
+        feedback:Elimina el enlace Sugerencias
+      */
+    //   $wp_admin_bar->remove_menu('wp-logo');
+    //   $wp_admin_bar->remove_menu('new-content');
+    //   $wp_admin_bar->remove_menu('comments');
+    }
+  endif;
+
+  add_action( 'wp_before_admin_bar_render', 'mawt_before_admin_bar' );
+
+  
